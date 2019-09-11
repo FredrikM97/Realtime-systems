0fd73549cb5801f00fffc5aec0e0ad3eb146c38e
Fredrik Mårtensson <fredrik9779@gmail.com>
2019-09-11 15:21:38 +0200

    Update lab0.c

--- part2/lab0.c
+++ part2/lab0.c
@@ -14,21 +14,6 @@ void man_sleep(int x){
 	} 
 }
 
-float power(float x, int y) { 
-    float temp; 
-    if( y == 0) 
-       return 1; 
-    temp = power(x, y/2);        
-    if (y%2 == 0) 
-        return temp*temp; 
-    else{ 
-        if(y > 0) 
-            return x*temp*temp; 
-        else
-            return (temp*temp)/x; 
-    } 
-}  
-
 int main()
 {
     /* Enable GPIO16 as an output */
