3813708d5832b7da7b44b27bb96da60678e6558a
emathal <54901220+emathal@users.noreply.github.com>
2019-09-18 13:44:33 +0200

    Update part1.c

--- Assigment2/part1.c
+++ Assigment2/part1.c
@@ -1,1 +1,32 @@
-Matte
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h> 
+#include "TaylorSeries.h"
+
+
+ExpStruct * iexp ( int x){
+	ExpStruct *output = (ExpStruct*) malloc(sizeof (ExpStruct)); //declare structure and allocate it to memory
+
+	int i; //init values
+	int n = abs(x)+1;
+	float sum = 1;
+	int fac = 1;
+
+	for(i = 1; i < n; i++){ //doing the Taylor Series one step at the time
+		fac *= i;
+		sum += pow(x,i)/fac;
+	}
+
+	output->expInt = (int)sum;//allocate the values to the structure
+	output->expFraction = (int)((sum - (int)sum)*100.0);
+
+	return output;//return the struktur
+}
+
+char *expToString(ExpStruct *iexp){
+	static char output[65];
+	
+	sprintf (output, "%d.%d", iexp->expInt, iexp->expFraction);
+	
+	return output;
+}
