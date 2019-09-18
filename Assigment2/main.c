781e08df1afcd8e91e3764ed57dd715107c0a911
emathal <54901220+emathal@users.noreply.github.com>
2019-09-18 13:45:39 +0200

    Update main.c

--- Assigment2/main.c
+++ Assigment2/main.c
@@ -1,1 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "TaylorSeries.h"
+#include <cstring>
+#include <assert.h>
+#include <time.h>
+#include <limits.h>
 
+int main (){	
+	expStruct *temp = iexp(4);
+	
+	printf("%d\n",temp->expInt);
+	printf("%s\n",expToString(temp));
+	
+	free(temp);
+	return 0;
+}
