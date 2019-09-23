5e0423e875101cb0ea80140881330a14a6ac3b8a
FredrikM97 <fredrik9779@gmail.com>
2019-09-23 16:20:02 +0200

    part2 finished

---
+++ Assignment2/part2/lab1/TaylorSeries.h
@@ -0,0 +1,10 @@
+struct expStruct {
+	int expInt;
+	int expFraction;
+};
+
+typedef struct expStruct ExpStruct;
+
+ExpStruct * iexp ( int );
+
+float data2pow(int ,int );
--- Assignment2/part2/lab1/lab1.c
+++ Assignment2/part2/lab1/lab1.c
@@ -5,23 +5,32 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <rpi3.h>
-
+#include "TaylorSeries.h"
+#include "piface.h"
 int main()
 {
     /* Enable GPIO16 as an output */
     GPIO->GPFSEL1 |= (1 << 18);
-    
+
     /* Turn LED on */
     GPIO->GPSET0 |= (1 << 16);
-    
-    /* Write to PiFace's LCD screen */    
+
+    /* Write to PiFace's LCD screen */
     piface_puts("Hello World!\n");
-    
+    //ExpStruct *temp;
+    int i=0;
+    char data[32];
     while (1)
     {
-        
+      ExpStruct *temp=iexp(i);
+      sprintf(data, "Data: %d.%d",temp->expInt,temp->expFraction);
+      piface_puts(data);
+      piface_delay();
+      piface_clear();
+      i++;
+      free(temp);
     }
-
 	return 0;
 }
