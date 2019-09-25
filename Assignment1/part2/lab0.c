ca4b3c7fbbb18e35fc0557134d60b87d4881a70f
FredrikM97 <fredrik9779@gmail.com>
2019-09-25 10:45:52 +0200

    part3

--- Assignment1/part2/lab0.c
+++ Assignment1/part2/lab0.c
@@ -4,32 +4,32 @@
     All rights reserved.
 */
 
-#include <rpi3.h> 
+#include <rpi3.h>
 #include <math.h>
 
 void man_sleep(int x){
 	int i;
 	for(i = 0;i < (x*10000); i++){
 		asm("nop");
-	} 
+	}
 }
 
 int main()
 {
     /* Enable GPIO16 as an output */
-    GPIO->GPFSEL1 |= (1 << 18); 
-    
+    GPIO->GPFSEL1 |= (1 << 18);
+
     /* Turn LED on */
-    GPIO->GPSET0 |= (1 << 16); 
+    GPIO->GPSET0 |= (1 << 16);
 	int i = 1;
-    while(1) 
+    while(1)
     {
         /* Iteratively turn on and off the LED */
 		GPIO->GPCLR0 |= (1 << 16);
 		man_sleep(i);
-		GPIO->GPSET0 |= (1 << 16); 
+		GPIO->GPSET0 |= (1 << 16);
 		man_sleep(i);
-		
+
 		if(i != -1) {
 			i = i<<1;
 		}
